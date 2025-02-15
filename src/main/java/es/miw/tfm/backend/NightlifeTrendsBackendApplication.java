e0722c488ae76fc514fc829e200cc977449e63a5
JAVIER ARANDA LOPEZ <javier.arandal@alumnos.upm.es>
2025-02-15 13:14:59 +0100

    Empty project #2

---
+++ src/main/java/es/miw/tfm/backend/NightlifeTrendsBackendApplication.java
@@ -0,0 +1,13 @@
+package es.miw.tfm.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NightlifeTrendsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NightlifeTrendsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/miw/tfm/backend/NightlifeTrendsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package es.miw.tfm.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NightlifeTrendsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
